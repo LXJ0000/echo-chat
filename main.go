0679f697d880b9d920f42fda7974835b44c23546
LXJ0000 <202100203017@stumail.sztu.edu.cn>
2024-01-24 13:33:06 +0800

    commit

--- main.go
+++ main.go
@@ -1,23 +1,6 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	//server := NewServer("127.0.0.1", 8888)
-	//server.Run()
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Println("请输入字符串：")
-	scanner.Scan()
-	line1 := scanner.Text()
-	fmt.Println("请输入字符串：")
-	scanner.Scan()
-	line2 := scanner.Text()
-
-	fmt.Println("1:", line1)
-	fmt.Println("2:", line2)
-
+	server := NewServer("127.0.0.1", 8888)
+	server.Run()
 }
